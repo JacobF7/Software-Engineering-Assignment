ebe09173999e5af8648ae58d5726e745903002ec
JacobF7 <jacob.falzon@gmail.com>
2015-04-16 17:56:18 +0200

    34th Commit
    Adding Comments to code

--- src/test/java/com/LeonAndJacob/app/Testing.java
+++ src/test/java/com/LeonAndJacob/app/Testing.java
@@ -4,7 +4,7 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.Assert;
-import org.powermock.core.classloader.annotations.PrepareForTest;
+
 
 /**
  * Unit test for simple App.
